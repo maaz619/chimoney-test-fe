23075ec0209b3e6f603987443fb56ff6237eca5d
maaz619 <maazahmedlfc@gmail.com>
2024-02-28 02:58:55 +0530

    added redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false
+    reactStrictMode: false,
+    redirects: async () => {
+        return [
+            {
+                source: '/',
+                destination: '/dashboard',
+                permanent: true,
+            },
+        ];
+    }
 };
 
 export default nextConfig;
